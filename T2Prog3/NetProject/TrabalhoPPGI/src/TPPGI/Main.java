af9a8a5e8e2c8c92e381498c541953da96003073
Gabriel Luiz de Oliveira Paschoal <gl.paschoal3@gmail.com>
2019-11-01 17:12:45 -0300

    Criando diretorio

---
+++ T2Prog3/NetProject/TrabalhoPPGI/src/TPPGI/Main.java
@@ -0,0 +1,11 @@
+package TPPGI;
+
+/**
+ *
+ * @author Gabriel Paschoal
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("---INICIO DE TUDO---");
+    }
+}
