dc7aa025b900f2315ace0efd6afe6819f2a57961
hiuri <hiuri3112@hotmail.com>
2019-11-26 14:59:42 -0200

    alteracao de leitura na main

--- T2Prog3/NetProject/TrabalhoPPGI/src/TPPGI/Main.java
+++ T2Prog3/NetProject/TrabalhoPPGI/src/TPPGI/Main.java
@@ -8,17 +8,33 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args){
+        String ano;
         System.out.println("---INICIO DE TUDO---");
         PPGI projeto = new PPGI();
         Scanner entrada = new Scanner(System.in);
-        projeto.LeDocentes("docentes.csv");
-        projeto.LeRegras("regras.csv");
-        projeto.leVeiculos("veiculos.csv");
-        projeto.LeQualis("qualis.csv");
-        projeto.LePublicacoes("publicacoes.csv");
-//        projeto.MostraDocentes();
-//        projeto.MostraRegras();
-//        projeto.MostraVeiculos();
+        for(int i=0; i<args.length;i++){
+            if(args[i].equals("-d")){
+                projeto.LeDocentes(args[i+1]);
+            }
+            else if(args[i].equals("-v")){
+                projeto.leVeiculos(args[i+1]);
+            }
+            else if(args[i].equals("-p")){
+                projeto.LePublicacoes(args[i+1]);
+            }
+            else if(args[i].equals("-q")){
+                projeto.LeQualis(args[i+1]);
+            }
+            else if(args[i].equals("-r")){
+                projeto.LeRegras(args[i+1]);
+            }
+            else if(args[i].equals("-a")){
+                ano=args[i+1];
+            }
+        }    
+       //projeto.MostraDocentes();
+       //projeto.MostraRegras();
+       //projeto.MostraVeiculos();
         projeto.MostraPublicacoes();
     }
     
